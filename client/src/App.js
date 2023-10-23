310aace0ad35fe81e9879d2f5c75dba633c35d53
SamirAlarconH <samiralarconhernandez@gmail.com>
2023-10-23 14:49:49 -0300

    DÏA 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import  Form  from '../Views/form/Form.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
+  return ( 
+    < div className = "App" >
+    <h1 >Henry Dogs </h1>
+    < BrowserRouter >
+    < Routes >
+    <Route> </Route>
+
+    </Routes> 
+    </BrowserRouter> 
     </div>
   );
 }
---
+++ client/src/Redux/Reducer/reducer.js
@@ -0,0 +1,12 @@
+//importacion action-types
+
+//definir el initialState
+
+let initialState = {
+    allDogs : [],
+    temperaments : []
+}
+
+//definir la funcion rooReducer
+
+ 
